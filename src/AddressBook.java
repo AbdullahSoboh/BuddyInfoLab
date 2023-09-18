267f6a04272ec748ecb2a0c0f91af20ceba9c207
Abdullah Soboh <abdullahsoboh@cmail.carleton.ca>
2023-09-17 20:50:54 -0400

    Main method

---
+++ src/AddressBook.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+
+public class AddressBook {
+
+    private ArrayList<BuddyInfo> buds;
+
+    public AddressBook() {
+        this.buds = new ArrayList<>();
+    }
+
+    public void addBud(BuddyInfo bud) {
+        if (bud != null) {
+            buds.add(bud);
+        }
+    }
+
+    public boolean removeBuddy(BuddyInfo bud) {
+        return buds.remove(bud);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Address Book");
+    }
+}
